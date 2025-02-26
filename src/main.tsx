f8b867113704f38c4eff608a5e1839a736ac64be
t.akasaki <t.akasaki.csharp@gmail.com>
2025-02-26 23:21:31 +0900

    feat: :sparkles: ReactRouterを追加

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import '@cloudscape-design/global-styles/index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
