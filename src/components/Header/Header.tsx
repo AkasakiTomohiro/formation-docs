62db9f02dac53d26112d3096feb355bcf223cc85
t.akasaki <t.akasaki.csharp@gmail.com>
2025-02-26 23:53:04 +0900

    feat: :sparkles: アプリヘッダーを作成

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import TopNavigation from '@cloudscape-design/components/top-navigation';
+import type { HeaderProps } from './types';
+
+export const Header = (props: HeaderProps): JSX.Element => {
+  return (
+    <>
+      <TopNavigation
+        identity={{
+          href: '/',
+          title: 'FormationDocs',
+        }}
+      />
+      {props.children}
+    </>
+  );
+};
