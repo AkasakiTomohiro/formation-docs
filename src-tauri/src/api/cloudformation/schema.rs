f3bb234024fef1249df8a805e741c87621339cb9
t.akasaki <t.akasaki.csharp@gmail.com>
2025-04-04 00:01:22 +0900

    refactor: :recycle: Zipダウンロード処理をリファクタ

---
+++ src-tauri/src/api/cloudformation/schema.rs
@@ -0,0 +1,81 @@
+use super::super::super::utils::AppError;
+use crate::command::app_config::APP_CONFIG_DIRECTORY_NAME;
+use std::{io::Cursor, path::PathBuf};
+use thiserror::Error;
+use zip::ZipArchive;
+
+use dirs::config_local_dir;
+
+#[derive(Debug, Error)]
+pub enum DlSchemaError {
+    #[error("app error: {0}")]
+    App(#[from] AppError),
+    #[error("reqwest error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+    #[error("io error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("zip error: {0:?}")]
+    Zip(#[from] zip::result::ZipError),
+}
+
+fn get_resource_provider_dl_path(region: &str) -> String {
+    return format!(
+        "https://schema.cloudformation.{}.amazonaws.com/CloudformationSchema.zip",
+        region
+    );
+}
+
+fn get_resource_provider_save_path(region: &str) -> Result<PathBuf, DlSchemaError> {
+    return match config_local_dir() {
+        Some(dir) => Ok(dir
+            .join(APP_CONFIG_DIRECTORY_NAME)
+            .join(format!("CloudformationSchema-{}", region))),
+        None => Err(DlSchemaError::App(AppError::new(
+            "Failed to get config local dir",
+        ))),
+    };
+}
+
+fn get_resource_provider_save_dir(region: &str) -> Result<PathBuf, DlSchemaError> {
+    return match config_local_dir() {
+        Some(dir) => Ok(dir
+            .join(APP_CONFIG_DIRECTORY_NAME)
+            .join("CloudformationSchema")
+            .join(region)),
+        None => Err(DlSchemaError::App(AppError::new(
+            "Failed to get config local dir",
+        ))),
+    };
+}
+
+pub async fn dl_resource_provider(region: &str) -> Result<(), DlSchemaError> {
+    let url = get_resource_provider_dl_path(region);
+    let save_path = get_resource_provider_save_path(region)?;
+    if save_path.exists() {
+        std::fs::remove_file(&save_path)?
+    }
+
+    // Zipファイルをダウンロード
+    let response = reqwest::get(url).await?;
+    let bytes = response.bytes().await?;
+
+    // ZIPファイルを解凍
+    let content = Cursor::new(bytes);
+    let output_dir = get_resource_provider_save_dir(region)?;
+    let mut archive = ZipArchive::new(content)?;
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let out_path = output_dir.join(file.name());
+
+        if file.is_dir() {
+            std::fs::create_dir_all(&out_path)?;
+        } else {
+            if let Some(parent) = out_path.parent() {
+                std::fs::create_dir_all(parent)?;
+            }
+            let mut outfile = std::fs::File::create(&out_path)?;
+            std::io::copy(&mut file, &mut outfile)?;
+        }
+    }
+    return Ok(());
+}
--- src-tauri/src/command/resource_provider.rs
+++ src-tauri/src/command/resource_provider.rs
@@ -1,122 +1,5 @@
+use super::super::api::cloudformation;
 use super::super::api::github;
-use std::{io::Cursor, path::PathBuf};
-use tokio::fs;
-use zip::ZipArchive;
-
-use dirs::config_local_dir;
-
-fn get_resource_provider_dl_path(region: &str) -> String {
-    return format!(
-        "https://schema.cloudformation.{}.amazonaws.com/CloudformationSchema.zip",
-        region
-    );
-}
-
-fn get_resource_provider_save_path(region: &str) -> Option<PathBuf> {
-    return match config_local_dir() {
-        Some(dir) => Some(
-            dir.join(super::app_config::APP_CONFIG_DIRECTORY_NAME)
-                .join(format!("CloudformationSchema-{}", region)),
-        ),
-        None => None,
-    };
-}
-
-fn get_resource_provider_save_dir(region: &str) -> Option<PathBuf> {
-    return match config_local_dir() {
-        Some(dir) => Some(
-            dir.join(super::app_config::APP_CONFIG_DIRECTORY_NAME)
-                .join("CloudformationSchema")
-                .join(region),
-        ),
-        None => None,
-    };
-}
-
-async fn get_resource_provider_dl(region: &str) -> Result<(), ()> {
-    let url = get_resource_provider_dl_path(region);
-    let save_path = match get_resource_provider_save_path(region) {
-        Some(path) => path,
-        None => {
-            println!("Failed to get config local dir");
-            return Err(());
-        }
-    };
-    if save_path.exists() {
-        if let Err(_) = fs::remove_file(&save_path).await {
-            println!("Failed to remove existing resource provider file");
-            return Err(());
-        }
-    }
-    let response = match reqwest::get(url).await {
-        Ok(res) => res,
-        Err(_) => {
-            println!("Failed to get resource provider");
-            return Err(());
-        }
-    };
-    let bytes = match response.bytes().await {
-        Ok(bytes) => bytes,
-        Err(_) => {
-            println!("Failed to read resource provider bytes");
-            return Err(());
-        }
-    };
-    let content = Cursor::new(bytes);
-
-    // ZIPファイルを解凍
-    let output_dir = match get_resource_provider_save_dir(region) {
-        Some(path) => path,
-        None => {
-            println!("Failed to get config local dir");
-            return Err(());
-        }
-    };
-    let mut archive = match ZipArchive::new(content) {
-        Ok(archive) => archive,
-        Err(_) => {
-            println!("Failed to create ZIP archive");
-            return Err(());
-        }
-    };
-    for i in 0..archive.len() {
-        let mut file = match archive.by_index(i) {
-            Ok(file) => file,
-            Err(_) => {
-                println!("Failed to get file from ZIP archive");
-                return Err(());
-            }
-        };
-        let out_path = output_dir.join(file.name());
-
-        if file.is_dir() {
-            if let Err(_) = std::fs::create_dir_all(&out_path) {
-                println!("Failed to create directory in output path");
-                return Err(());
-            };
-        } else {
-            if let Some(parent) = out_path.parent() {
-                if let Err(_) = std::fs::create_dir_all(parent) {
-                    println!("Failed to create parent directory in output path");
-                    return Err(());
-                };
-            }
-            let mut outfile = match std::fs::File::create(&out_path) {
-                Ok(file) => file,
-                Err(_) => {
-                    println!("Failed to create file in output directory");
-                    return Err(());
-                }
-            };
-            if let Err(_) = std::io::copy(&mut file, &mut outfile) {
-                println!("Failed to copy file to output directory");
-                return Err(());
-            };
-        }
-    }
-
-    Ok(())
-}
 
 async fn get_service_data_dl() -> Result<(), ()> {
     let branch = github::branch::get_branch("aws", "aws-cli", "v2")
@@ -137,7 +20,7 @@ async fn get_service_data_dl() -> Result<(), ()> {
 
 #[tauri::command]
 pub async fn setup_app() -> Result<(), ()> {
-    if let Err(_) = get_resource_provider_dl("us-east-1").await {
+    if let Err(_) = cloudformation::schema::dl_resource_provider("us-east-1").await {
         return Err(());
     }
     if let Err(_) = get_service_data_dl().await {
