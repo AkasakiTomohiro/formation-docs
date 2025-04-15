6781ea3e475e07f3a62bb2984583933936a2a451
a.kubo <ayuyellow1725@gmail.com>
2025-04-15 22:52:55 +0900

    feat: :sparkles: aws-cliをZipダウンロードしservice-2.jsonを保存する機能を追加

--- src-tauri/src/api/github/mod.rs
+++ src-tauri/src/api/github/mod.rs
@@ -1,1 +1,1 @@
-pub mod branch;
+pub mod repos;
--- src-tauri/src/command/resource_provider.rs
+++ src-tauri/src/command/resource_provider.rs
@@ -1,8 +1,15 @@
+use std::io::Cursor;
+use std::path::PathBuf;
+
+use crate::api::github::repos::dl_zip_file;
+use crate::api::github::repos::GithubReposError;
 use crate::command::app_config::APP_CONFIG_DIRECTORY_NAME;
 use crate::utils::AppError;
 use dirs::config_local_dir;
+use globset::Glob;
+use regex::Regex;
 use thiserror::Error;
-use tokio::fs;
+use zip::ZipArchive;
 
 use super::super::api::cloudformation;
 use super::super::api::github;
@@ -17,13 +24,31 @@ pub enum GetServiceDataDownloadError {
     App(#[from] AppError),
     #[error("octocrab error: {0}")]
     Octocrab(#[from] octocrab::Error),
+    #[error("io error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("github repos error: {0:?}")]
+    GithubRepo(#[from] GithubReposError),
+    #[error("zip error: {0:?}")]
+    Zip(#[from] zip::result::ZipError),
+    #[error("globset error: {0:?}")]
+    Globset(#[from] globset::Error),
+}
+
+fn get_service_data_save_dir() -> Result<PathBuf, GetServiceDataDownloadError> {
+    return match config_local_dir() {
+        Some(dir) => Ok(dir.join(APP_CONFIG_DIRECTORY_NAME).join("aws-cli")),
+        None => Err(GetServiceDataDownloadError::App(AppError::new(
+            "Failed to get config local dir",
+        ))),
+    };
 }
 
 async fn get_service_data_dl() -> Result<(), GetServiceDataDownloadError> {
-    let branch = github::branch::get_branch("aws", "aws-cli", "v2")
+    let branch = github::repos::get_branch("aws", "aws-cli", "v2")
         .await
         .unwrap();
 
+    // コミットハッシュ取得
     let commit_sha = match branch["commit"]["sha"].as_str() {
         Some(sha) => sha,
         None => {
@@ -35,58 +60,47 @@ async fn get_service_data_dl() -> Result<(), GetServiceDataDownloadError> {
     };
     println!("Branch commit SHA: {:?}", commit_sha);
 
-    let list = octocrab::instance()
-        .repos(AWS_CLI_OWNER, AWS_CLI_REPO)
-        .get_content()
-        .path(AWS_CLI_PATH)
-        .r#ref(commit_sha)
-        .send()
-        .await?;
-    let service = octocrab::instance()
-        .repos(AWS_CLI_OWNER, AWS_CLI_REPO)
-        .get_content()
-        .path(format!("{}/{}", AWS_CLI_PATH, list.items[0].name))
-        .r#ref(commit_sha)
-        .send()
-        .await?;
-    let mut content = octocrab::instance()
-        .repos(AWS_CLI_OWNER, AWS_CLI_REPO)
-        .get_content()
-        .path(format!(
-            "{}/{}/{}/{}",
-            AWS_CLI_PATH, list.items[0].name, service.items[0].name, "service-2.json"
-        ))
-        .r#ref(commit_sha)
-        .send()
-        .await?;
-    let contents = content.take_items();
-    let c = &contents[0];
-    if c.r#type != "file" {
-        return Err(GetServiceDataDownloadError::App(AppError::new(
-            "Failed to get service-2.json",
-        )));
-    }
-    let decoded_content = c.decoded_content().unwrap();
-    println!("Decoded content");
+    // FIXME: コミットハッシュが保存されているものと一致しない場合のみダウンロードするように修正
 
-    let save_path = match config_local_dir() {
-        Some(dir) => Some(dir.join(APP_CONFIG_DIRECTORY_NAME).join("aws-cli")),
-        None => None,
-    };
-    let save_path = save_path.unwrap();
-    println!("{:?}", save_path);
-    std::fs::create_dir_all(&save_path);
-    return match fs::write(
-        save_path.join(format!("{}.json", list.items[0].name)),
-        decoded_content,
-    )
-    .await
-    {
-        Ok(_) => Ok(()),
-        Err(_) => Err(GetServiceDataDownloadError::App(AppError::new(
-            "Failed to write service-2.json",
-        ))),
-    };
+    // aws-cliのリポジトリをZipダウンロード
+    let bytes = dl_zip_file(AWS_CLI_OWNER, AWS_CLI_REPO, commit_sha).await?;
+
+    // Zipファイルを解凍&ファイル保存
+    let content = Cursor::new(bytes);
+    let output_dir = get_service_data_save_dir()?;
+    std::fs::create_dir_all(&output_dir)?;
+    let mut archive = ZipArchive::new(content)?;
+
+    let glob =
+        Glob::new(format!("**/{}/*/*/service-2.json", AWS_CLI_PATH).as_str())?.compile_matcher();
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+
+        if file.is_file() && glob.is_match(file.name()) {
+            // `awscli/botocore/data/` の直後のディレクトリ名をキャプチャ
+            let re = Regex::new(format!(r"{}/([^/]+)/", AWS_CLI_PATH).as_str()).unwrap();
+            // キャプチャグループに一致する部分(サービス名)を取得
+            let service_name = re
+                .captures(file.name())
+                .and_then(|caps| caps.get(1).map(|m| m.as_str().to_string()));
+            let service_name = match service_name {
+                Some(name) => name,
+                None => {
+                    println!("Failed to get service name from file name: {}", file.name());
+                    return Err(GetServiceDataDownloadError::App(AppError::new(
+                        "Failed to get service name from file name",
+                    )));
+                }
+            };
+
+            // ファイルを保存
+            let mut outfile =
+                std::fs::File::create(output_dir.join(format!("{}.json", service_name)))?;
+            std::io::copy(&mut file, &mut outfile)?;
+        }
+    }
+
+    return Ok(());
 }
 
 #[tauri::command]
