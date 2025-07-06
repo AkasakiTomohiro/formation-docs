96c09218808cf2d99dcd23ed43174b352919733d
a.kubo <ayuyellow1725@gmail.com>
2025-07-06 13:19:24 +0900

    fix: :bug: 手動管理リソースのリストに選択した種別のリソースのみが表示されるように修正

--- src/invoke/ManualManagementResource.ts
+++ src/invoke/ManualManagementResource.ts
@@ -24,18 +24,23 @@ export async function newManualManagementResource(
   return result.value;
 }
 
+export type ManualManagementResourceListProps = {
+  service_name: string;
+  resource_name: string;
+};
 export type ManualManagementResource = {
   resourceId: string;
   type: string;
   description: string;
 };
 
-export async function getManualManagementResourceList(): Promise<
-  ManualManagementResource[]
-> {
+export async function getManualManagementResourceList(
+  props: ManualManagementResourceListProps,
+): Promise<ManualManagementResource[]> {
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   const result = await invoke<CommandResult<any>>(
     'get_manual_management_resource_list_command',
+    props,
   );
   if (!result.success) {
     throw new Error(result.value);
