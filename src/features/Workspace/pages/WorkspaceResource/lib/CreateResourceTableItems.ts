b724ea8266887a0892a100920e4b4bd5bed2f966
t.akasaki <t.akasaki.csharp@gmail.com>
2025-10-05 15:13:06 +0900

    feat: :sparkles: childrenPropertiesがundefinedのときプロパティが表示されないバグを修正

--- src/features/Workspace/pages/WorkspaceResource/lib/CreateResourceTableItems.ts
+++ src/features/Workspace/pages/WorkspaceResource/lib/CreateResourceTableItems.ts
@@ -261,14 +261,14 @@ function parseReferencePropertyToTableItem(
  * @return テーブルアイテム
  */
 function parseChildrenPropertyToTableItem(
-  childrenProperties: Record<string, Property>,
+  childrenProperties: Record<string, Property> | undefined,
   parentId: string,
   actualProperties: any,
   definitions: CloudFormationSchema['definitions'] | undefined,
   options: CreateResourceTableItemsOption,
 ): ResourceTableItem[] | undefined {
   const result: ResourceTableItem[] = [];
-  for (const [definitionKey, definitionValue] of Object.entries(childrenProperties)) {
+  for (const [definitionKey, definitionValue] of Object.entries(childrenProperties ?? {})) {
     // $refを含む場合は、参照プロパティを取得する
     if ('$ref' in definitionValue) {
       const childItem = parseReferencePropertyToTableItem(
