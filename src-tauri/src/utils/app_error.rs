979a22aa4e43086b237f6ff69b91f5c883f54ffe
a.kubo <ayuyellow1725@gmail.com>
2025-11-23 17:08:28 +0900

    test: :white_check_mark: `AppError`のUT作成

--- src-tauri/src/utils/app_error.rs
+++ src-tauri/src/utils/app_error.rs
@@ -19,3 +19,34 @@ impl Display for AppError {
         write!(f, "{}", self.message)
     }
 }
+
+#[cfg(test)]
+#[coverage(off)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn app_error_new() {
+        // ######### 準備 #########
+        let error_message = "error message;";
+
+        // ######### 実行 #########
+        let app_error = AppError::new(error_message);
+
+        // ######### 検証 #########
+        assert_eq!(app_error.to_string(), error_message);
+    }
+
+    #[test]
+    fn app_error_display() {
+        // ######### 準備 #########
+        let error_message = "test error message";
+        let app_error = AppError::new(error_message);
+
+        // ######### 実行 #########
+        let formatted = format!("Error: {}", app_error);
+
+        // ######### 検証 #########
+        assert_eq!(formatted, "Error: test error message");
+    }
+}
