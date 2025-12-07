a0a737618d5e527c7bbcb62bd5ac55b229fb5773
Yoshida010 <kamuizu0623@gmail.com>
2025-12-07 16:35:40 +0900

    test: :construction: resource_providerのUT作成途中

--- src-tauri/src/command/resource_provider.rs
+++ src-tauri/src/command/resource_provider.rs
@@ -57,3 +57,96 @@ pub async fn setup_app_command(
         Err(_) => Err(()),
     }
 }
+
+#[cfg(test)]
+mod setup_app_tests {
+    use chrono::DateTime;
+
+    use super::*;
+    use std::{collections::HashMap, sync::Arc};
+
+    use crate::{
+        api::context::{
+            api_context::ApiContext, cloudformation_schema_trait::MockCloudformationSchemaTrait,
+        },
+        config::{
+            app_config::AppConfig,
+            context::{
+                app_config_trait::MockAppConfigTrait, config_context::ConfigContext,
+                stack_meta_config_trait::MockStackMetaConfigTrait,
+                workspace_config_trait::MockWorkspaceConfigTrait,
+            },
+        },
+        utils::context::{
+            app_context::AppContext, file::MockFileSystem, http_client::MockHttpClient,
+        },
+    };
+
+    /// app_configが初期化されていない場合、初期化されること
+    #[tokio::test]
+    async fn setup_app_initialized() {
+        // ######### 準備 #########
+        let mock_file_system = MockFileSystem::new();
+        let mock_http_client = MockHttpClient::new();
+
+        let app_context = AppContext {
+            file_system: Arc::new(mock_file_system),
+            http_client: Arc::new(mock_http_client),
+        };
+
+        let mut mock_app_config = MockAppConfigTrait::new();
+
+        // AppConfig::readのモック
+        mock_app_config.expect_read().returning(|_| {
+            let app_config = AppConfig {
+                version: 1,
+                initialized: false,
+                initialized_at: String::from(""),
+                workspaces: HashMap::new(),
+            };
+            return Ok(app_config);
+        });
+
+        // AppConfig::writeのモック
+        mock_app_config
+            .expect_write()
+            // FIXME: 以下のwithfでinitialized_atのフォーマットチェックを入れたいが、うまく動作しないため一旦コメントアウト
+            // .withf(|app_config, _| {
+            //     match DateTime::parse_from_rfc3339(&app_config.initialized_at) {
+            //         Ok(_) => {
+            //             return app_config.initialized == true;
+            //         }
+            //         Err(_) => return false,
+            //     };
+            // })
+            .returning(|app_config, _| {
+                return Ok(app_config);
+            });
+
+        let mock_stack_meta_config = MockStackMetaConfigTrait::new();
+        let mock_workspace_config_io = MockWorkspaceConfigTrait::new();
+
+        let config_context = ConfigContext {
+            app_config_io: Arc::new(mock_app_config),
+            workspace_config_io: Arc::new(mock_workspace_config_io),
+            stack_meta_config_io: Arc::new(mock_stack_meta_config),
+        };
+
+        let mut mock_cloudformation_schema = MockCloudformationSchemaTrait::new();
+
+        // CloudformationSchema::dl_resource_providerのモック
+        mock_cloudformation_schema
+            .expect_dl_resource_provider()
+            .returning(|_, _| Ok(()));
+
+        let api_context = ApiContext {
+            cloudformation_schema: Arc::new(mock_cloudformation_schema),
+        };
+
+        // ######### 実行 #########
+        let initialized = setup_app(&app_context, &config_context, &api_context).await;
+
+        // ######### 検証 #########
+        assert!(initialized.is_ok());
+    }
+}
