f7dd004c791e433271771d4f7e191e5c681317bb
nakao <yukimiruku1126@gmail.com>
2025-12-14 13:48:27 +0900

    test: :white_check_mark: `command/cloudformation_schema`の`get_aws_service_list`関数のUTを実装

--- src-tauri/src/command/cloudformation_schema.rs
+++ src-tauri/src/command/cloudformation_schema.rs
@@ -308,4 +308,341 @@ mod get_cloudformation_schema_tests {
         // get_cloudformation_schemaの戻り値がErrであること
         assert!(result.is_err());
     }
+
+    /// aws service listを取得できることを確認
+    #[tokio::test]
+    async fn get_aws_service_list_success() {
+        // ######### 準備 #########
+        let mut mock_file_system: MockFileSystem = MockFileSystem::new();
+
+        let mut mock_cloudformation_schema: MockCloudformationSchemaTrait =
+            MockCloudformationSchemaTrait::new();
+
+        let expected_service_list: HashMap<String, Vec<String>> = HashMap::from([
+            (
+                "ServiceA".to_string(),
+                vec!["Resource1".to_string(), "Resource2".to_string()],
+            ),
+            ("ServiceB".to_string(), vec!["Resource3".to_string()]),
+        ]);
+
+        // get_resource_provider_save_dirのモック
+        let schema_directory = PathBuf::from_str("schema_directory").unwrap();
+        let schema_directory_clone = schema_directory.clone();
+        mock_cloudformation_schema
+            .expect_get_resource_provider_save_dir()
+            .returning(move |_file_system, _region| {
+                return Ok(schema_directory_clone.clone());
+            });
+
+        // path_existsのモック
+        mock_file_system.expect_path_exists().return_const(true);
+
+        // read_fileのモック
+        mock_file_system
+            .expect_read_file()
+            .withf(move |file_path| {
+                let summary_file_path = schema_directory
+                    .clone()
+                    .join(cloudformation::schema::SUMMARY_SERVICE_LIST_FILE);
+
+                return file_path == &summary_file_path;
+            })
+            .returning(|_path| {
+                let summary_json = r#"{
+                    "ServiceA": ["Resource1", "Resource2"],
+                    "ServiceB": ["Resource3"]
+                }"#;
+                return Ok(summary_json.to_string());
+            });
+
+        let mock_http_client = MockHttpClient::new();
+
+        let app_context = AppContext {
+            file_system: Arc::new(mock_file_system),
+            http_client: Arc::new(mock_http_client),
+        };
+
+        let api_context = ApiContext {
+            cloudformation_schema: Arc::new(mock_cloudformation_schema),
+        };
+
+        // ######### 実行 #########
+        let result = get_aws_service_list(&app_context, &api_context).await;
+
+        // ######### 検証 #########
+        // aws service listが取得できること
+        assert!(result.is_ok());
+        let service_list = result.unwrap();
+        assert_eq!(service_list.len(), 2);
+        assert_eq!(
+            service_list.get("ServiceA").unwrap(),
+            expected_service_list.get("ServiceA").unwrap()
+        );
+        assert_eq!(
+            service_list.get("ServiceB").unwrap(),
+            expected_service_list.get("ServiceB").unwrap()
+        );
+    }
+
+    /// path_existsがfalseの場合、generate_summary_service_listが呼ばれることを確認
+    #[tokio::test]
+    async fn get_aws_service_list_generate_summary() {
+        // ######### 準備 #########
+        let mut mock_file_system: MockFileSystem = MockFileSystem::new();
+
+        let mut mock_cloudformation_schema: MockCloudformationSchemaTrait =
+            MockCloudformationSchemaTrait::new();
+
+        let expected_service_list: HashMap<String, Vec<String>> = HashMap::from([
+            (
+                "ServiceA".to_string(),
+                vec!["Resource1".to_string(), "Resource2".to_string()],
+            ),
+            ("ServiceB".to_string(), vec!["Resource3".to_string()]),
+        ]);
+
+        // get_resource_provider_save_dirのモック
+        let schema_directory = PathBuf::from_str("schema_directory").unwrap();
+        let schema_directory_clone = schema_directory.clone();
+        mock_cloudformation_schema
+            .expect_get_resource_provider_save_dir()
+            .returning(move |_file_system, _region| {
+                return Ok(schema_directory_clone.clone());
+            });
+
+        // path_existsのモック
+        mock_file_system.expect_path_exists().return_const(false);
+
+        // generate_summary_service_listのモック
+        let schema_directory_clone2 = schema_directory.clone();
+        mock_cloudformation_schema
+            .expect_generate_summary_service_list()
+            .withf(move |_cxt, output_dir| {
+                return output_dir == &schema_directory_clone2;
+            })
+            .returning(|_cxt, _output_dir| Ok(()));
+
+        // read_fileのモック
+        mock_file_system
+            .expect_read_file()
+            .withf(move |file_path| {
+                let summary_file_path = schema_directory
+                    .clone()
+                    .join(cloudformation::schema::SUMMARY_SERVICE_LIST_FILE);
+
+                return file_path == &summary_file_path;
+            })
+            .returning(|_path| {
+                let summary_json = r#"{
+                    "ServiceA": ["Resource1", "Resource2"],
+                    "ServiceB": ["Resource3"]
+                }"#;
+                return Ok(summary_json.to_string());
+            });
+
+        let mock_http_client = MockHttpClient::new();
+
+        let app_context = AppContext {
+            file_system: Arc::new(mock_file_system),
+            http_client: Arc::new(mock_http_client),
+        };
+
+        let api_context = ApiContext {
+            cloudformation_schema: Arc::new(mock_cloudformation_schema),
+        };
+
+        // ######### 実行 #########
+        let result = get_aws_service_list(&app_context, &api_context).await;
+
+        // ######### 検証 #########
+        // aws service listが取得できること
+        assert!(result.is_ok());
+        let service_list = result.unwrap();
+        assert_eq!(service_list.len(), 2);
+        assert_eq!(
+            service_list.get("ServiceA").unwrap(),
+            expected_service_list.get("ServiceA").unwrap()
+        );
+        assert_eq!(
+            service_list.get("ServiceB").unwrap(),
+            expected_service_list.get("ServiceB").unwrap()
+        );
+    }
+
+    // get_resource_provider_save_dirがErrの場合、get_aws_service_listがErrを返すことを確認
+    #[tokio::test]
+    async fn get_aws_service_list_err_get_resource_provider() {
+        // ######### 準備 #########
+        let mock_file_system: MockFileSystem = MockFileSystem::new();
+        let mut mock_cloudformation_schema: MockCloudformationSchemaTrait =
+            MockCloudformationSchemaTrait::new();
+
+        // get_resource_provider_save_dirのモック
+        mock_cloudformation_schema
+            .expect_get_resource_provider_save_dir()
+            .returning(move |_file_system, _region| {
+                Err(DlSchemaError::App(AppError::new(
+                    "get_resource_provider_save_dir error",
+                )))
+            });
+
+        let mock_http_client = MockHttpClient::new();
+
+        let app_context = AppContext {
+            file_system: Arc::new(mock_file_system),
+            http_client: Arc::new(mock_http_client),
+        };
+
+        let api_context = ApiContext {
+            cloudformation_schema: Arc::new(mock_cloudformation_schema),
+        };
+
+        // ######### 実行 #########
+        let result = get_aws_service_list(&app_context, &api_context).await;
+
+        // ######### 検証 #########
+        // get_aws_service_listの戻り値がErrであること
+        assert!(result.is_err());
+    }
+
+    // generate_summary_service_listがErrの場合、get_aws_service_listがErrを返すことを確認
+    #[tokio::test]
+    async fn get_aws_service_list_err_generate_summary_service_list() {
+        // ######### 準備 #########
+        let mut mock_file_system: MockFileSystem = MockFileSystem::new();
+
+        let mut mock_cloudformation_schema: MockCloudformationSchemaTrait =
+            MockCloudformationSchemaTrait::new();
+
+        // get_resource_provider_save_dirのモック
+        let schema_directory = PathBuf::from_str("schema_directory").unwrap();
+        let schema_directory_clone = schema_directory.clone();
+        mock_cloudformation_schema
+            .expect_get_resource_provider_save_dir()
+            .returning(move |_file_system, _region| {
+                return Ok(schema_directory_clone.clone());
+            });
+
+        // path_existsのモック
+        mock_file_system.expect_path_exists().return_const(false);
+
+        // generate_summary_service_listのモック
+        mock_cloudformation_schema
+            .expect_generate_summary_service_list()
+            .returning(|_cxt, _output_dir| {
+                Err(DlSchemaError::App(AppError::new(
+                    "generate_summary_service_list error",
+                )))
+            });
+
+        let mock_http_client = MockHttpClient::new();
+
+        let app_context = AppContext {
+            file_system: Arc::new(mock_file_system),
+            http_client: Arc::new(mock_http_client),
+        };
+
+        let api_context = ApiContext {
+            cloudformation_schema: Arc::new(mock_cloudformation_schema),
+        };
+
+        // ######### 実行 #########
+        let result = get_aws_service_list(&app_context, &api_context).await;
+
+        // ######### 検証 #########
+        // get_aws_service_listの戻り値がErrであること
+        assert!(result.is_err());
+    }
+
+    // read_fileの戻り値がErrの場合、get_aws_service_listがErrを返すことを確認
+    #[tokio::test]
+    async fn get_aws_service_list_err_read_file() {
+        // ######### 準備 #########
+        let mut mock_file_system: MockFileSystem = MockFileSystem::new();
+
+        let mut mock_cloudformation_schema: MockCloudformationSchemaTrait =
+            MockCloudformationSchemaTrait::new();
+
+        // get_resource_provider_save_dirのモック
+        let schema_directory = PathBuf::from_str("schema_directory").unwrap();
+        let schema_directory_clone = schema_directory.clone();
+        mock_cloudformation_schema
+            .expect_get_resource_provider_save_dir()
+            .returning(move |_file_system, _region| {
+                return Ok(schema_directory_clone.clone());
+            });
+
+        // path_existsのモック
+        mock_file_system.expect_path_exists().return_const(true);
+
+        // read_fileのモック
+        mock_file_system
+            .expect_read_file()
+            .returning(|_path| Err(Error::new(ErrorKind::Other, "read_file error")));
+
+        let mock_http_client = MockHttpClient::new();
+
+        let app_context = AppContext {
+            file_system: Arc::new(mock_file_system),
+            http_client: Arc::new(mock_http_client),
+        };
+
+        let api_context = ApiContext {
+            cloudformation_schema: Arc::new(mock_cloudformation_schema),
+        };
+
+        // ######### 実行 #########
+        let result = get_aws_service_list(&app_context, &api_context).await;
+
+        // ######### 検証 #########
+        // get_aws_service_listの戻り値がErrであること
+        assert!(result.is_err());
+    }
+
+    // serde_json::from_strがErrの場合、get_aws_service_listがErrを返すことを確認
+    #[tokio::test]
+    async fn get_aws_service_list_err_json_from_str() {
+        // ######### 準備 #########
+        let mut mock_file_system: MockFileSystem = MockFileSystem::new();
+
+        let mut mock_cloudformation_schema: MockCloudformationSchemaTrait =
+            MockCloudformationSchemaTrait::new();
+
+        // get_resource_provider_save_dirのモック
+        let schema_directory = PathBuf::from_str("schema_directory").unwrap();
+        let schema_directory_clone = schema_directory.clone();
+        mock_cloudformation_schema
+            .expect_get_resource_provider_save_dir()
+            .returning(move |_file_system, _region| {
+                return Ok(schema_directory_clone.clone());
+            });
+
+        // path_existsのモック
+        mock_file_system.expect_path_exists().return_const(true);
+
+        // read_fileのモック
+        mock_file_system.expect_read_file().returning(|_path| {
+            let summary_json = r#"invalid json"#;
+            return Ok(summary_json.to_string());
+        });
+
+        let mock_http_client = MockHttpClient::new();
+
+        let app_context = AppContext {
+            file_system: Arc::new(mock_file_system),
+            http_client: Arc::new(mock_http_client),
+        };
+
+        let api_context = ApiContext {
+            cloudformation_schema: Arc::new(mock_cloudformation_schema),
+        };
+
+        // ######### 実行 #########
+        let result = get_aws_service_list(&app_context, &api_context).await;
+
+        // ######### 検証 #########
+        // get_aws_service_listの戻り値がErrであること
+        assert!(result.is_err());
+    }
 }
