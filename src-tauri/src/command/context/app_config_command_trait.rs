9373f748f10e36587b8f39f576fcc000b1269151
a.kubo <ayuyellow1725@gmail.com>
2026-01-08 22:43:12 +0900

    refactor: :recycle: Trait名変更

--- src-tauri/src/command/context/app_config_command_trait.rs
+++ src-tauri/src/command/context/app_config_command_trait.rs
@@ -2,9 +2,9 @@ use async_trait::async_trait;
 
 #[mockall::automock]
 #[async_trait]
-pub trait AppConfigTrait: Send + Sync {}
+pub trait AppConfigCommandTrait: Send + Sync {}
 
 pub struct AppConfigCommand;
 #[coverage(off)]
 #[async_trait]
-impl AppConfigTrait for AppConfigCommand {}
+impl AppConfigCommandTrait for AppConfigCommand {}
--- src-tauri/src/command/context/mod.rs
+++ src-tauri/src/command/context/mod.rs
@@ -1,3 +1,3 @@
-pub mod app_config_trait;
+pub mod app_config_command_trait;
 pub mod command_context;
-pub mod stack_trait;
+pub mod stack_command_trait;
