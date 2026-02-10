b04b49eb02b8d4adf46e1bf4047552e6a429ae1f
Yoshida010 <kamuizu0623@gmail.com>
2026-02-10 22:54:41 +0900

    feat: :construction: `get_latest_version`関数の作成完了

--- src-tauri/src/api/get_latest_version/get_latest_version.rs
+++ src-tauri/src/api/get_latest_version/get_latest_version.rs
@@ -4,24 +4,15 @@ pub const LATEST_VERSION_URL: &str = "https://raw.githubusercontent.com/AkasakiT
 
 pub async fn get_latest_version(ctx: &AppContext) -> Option<String> {
     // version.jsonを取得
-    let latest_version_json = ctx.http_client.get(LATEST_VERSION_URL.to_string()).await;
-    if latest_version_json.is_err() {
-        return None;
-    }
-    return match serde_json::from_slice::<serde_json::Value>(&latest_version_json.unwrap()) {
-        Ok(json) => {
-            let version = json.get("version");
-            if version.is_none() {
-                return None;
-            }
-            let version = version.unwrap();
-            if !version.is_string() {
-                return None;
-            }
-            return Some(version.as_str().unwrap().to_string());
-        }
-        Err(err) => {
-            return None;
-        }
-    };
+    let response = ctx
+        .http_client
+        .get(LATEST_VERSION_URL.to_string())
+        .await
+        .ok()?;
+
+    // 取得結果をデシリアライズ
+    let json = serde_json::from_slice::<serde_json::Value>(&response).ok()?;
+
+    // versionを返す
+    return Some(json.get("version")?.as_str()?.to_string());
 }
